738b5515e0578ffdbcca88b9862f4388f782b772
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-05-02 08:18:38 -0700

    w1: added Model helper methods

--- app/models/task.py
+++ app/models/task.py
@@ -1,5 +1,30 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from ..db import db
+from datetime import datetime
+from typing import Optional
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+    completed_at: Mapped[Optional[datetime]] = mapped_column(nullable=True)
+
+    # converts a Task instance into a dict 
+    # for generating JSON response
+    def to_dict(self): 
+        task_as_dict = {}
+        task_as_dict["id"] = self.id
+        task_as_dict["title"] = self.title
+        task_as_dict["description"] = self.description
+        task_as_dict["completed_at"] = self.completed_at
+
+        return task_as_dict
+    
+    # creates a new Task model instance from a dictionary
+    @classmethod 
+    def from_dict(cls, task_data):
+        new_task = Task(title=task_data["title"],
+                        description=task_data["description"],
+                        completed_at=task_data["completed_at"])
+        return new_task
+    
