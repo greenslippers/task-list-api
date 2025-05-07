d9d71c2448a32cbb1c697c8310cc5ae244ffda8d
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-05-07 11:29:01 -0700

    Adds one to many relationship and migration

--- app/models/goal.py
+++ app/models/goal.py
@@ -1,16 +1,26 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..db import db
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.models.task import Task 
 
 class Goal(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
+    tasks: Mapped[list["Task"]] = relationship(back_populates="goal")
 
     # converts a Goal instance into a dict 
     # for generating JSON response
     def to_dict(self): 
-        goal_as_dict = {}
-        goal_as_dict["id"] = self.id
-        goal_as_dict["title"] = self.title
+        goal_as_dict = {
+            "id": self.id,
+            "title": self.title
+        }
+
+        if self.tasks:
+            goal_as_dict["tasks"] = [
+                task.to_dict() for task in self.tasks
+                ]
 
         return goal_as_dict
     
--- app/models/task.py
+++ app/models/task.py
@@ -1,13 +1,19 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey
 from ..db import db
 from datetime import datetime
-# from typing import Optional
+from typing import Optional
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from app.models.goal import Goal 
 
 class Task(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     title: Mapped[str]
     description: Mapped[str]
     completed_at: Mapped[datetime] = mapped_column(nullable=True)
+    goal_id: Mapped[Optional[int]] = mapped_column(ForeignKey("goal.id"))
+    goal: Mapped[Optional["Goal"]] = relationship(back_populates="tasks")
 
     # converts a Task instance into a dict 
     # for generating JSON response
@@ -18,14 +24,22 @@ class Task(db.Model):
         task_as_dict["description"] = self.description
         task_as_dict["is_complete"] = self.completed_at is not None
 
+        if self.goal:
+            task_as_dict["goal"] = self.goal.name
+
         return task_as_dict
     
     # creates a new Task model instance from a dictionary
     @classmethod 
     def from_dict(cls, task_data):
-        new_task = Task(title=task_data["title"],
-                        description=task_data["description"],
-                        completed_at=task_data.get("completed_at")) # use .get()
-                                                        # to allow missing field
+        goal_id = task_data.get("goal_id")
+
+        new_task = cls(
+            title=task_data["title"],
+            description=task_data["description"],
+            completed_at=task_data.get("completed_at"), # use .get() to allow missing field
+            goal_id=goal_id
+            )
+
         return new_task
     
